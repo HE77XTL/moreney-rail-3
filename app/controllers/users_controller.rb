7b11294b40d76e7fed8750d9b4bfd6cd9796b393
HE77XTL <he77505357@163.com>
2020-08-11 23:08:04 +0800

    优化user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def create
+    render_resource User.create create_params
+  end
+  def create_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end

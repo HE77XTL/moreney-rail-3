9f49ded8a64d531e3566b8b7cb2ca0dafd5c2271
HE77XTL <he77505357@163.com>
2020-08-11 22:37:29 +0800

    I18n 验证提示国际化

---
+++ config/initializers/locale.rb
@@ -0,0 +1,8 @@
+# # Permitted locales available for the application
+# I18n.available_locales = [:en, :pt]
+#
+# # Set default locale to something other than :en
+# I18n.default_locale = :pt
+
+I18n.available_locales = [:en, "zh-CN"]
+I18n.default_locale = 'zh-CN'
